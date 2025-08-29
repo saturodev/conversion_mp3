3fc23cd33a24d95cddc906ea79edba4f0796559d
saturodev <rp1857465@gmail.com>
2025-08-29 12:02:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from handlers import setup_routers
 from middlewares.middleware import LoggingMiddleware
+from utils.database.create_db import create_tables
 
 dp = Dispatcher()
 dp.message.middleware(LoggingMiddleware())
@@ -12,6 +13,7 @@ setup_routers(dp)
 
 # Run the bot
 async def main() -> None:
+    create_tables()
     bot = Bot(token=config.TOKEN)
     await dp.start_polling(bot)
 
