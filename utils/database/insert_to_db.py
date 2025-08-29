159be5b07a105d47bcb7eff33813e60100ef1dc9
saturodev <rp1857465@gmail.com>
2025-08-29 12:06:35 +0500

    update insert_to_db.py

--- utils/database/insert_to_db.py
+++ utils/database/insert_to_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("mydatabase.db")
+conn = sqlite3.connect("data/mydatabase.db")
 cursor = conn.cursor()
 
 
@@ -17,15 +17,15 @@ def get_user_by_id(user_id):
     return cursor.fetchone()
 
 
-def add_to_music(user_id, title, file_id, url_id):
+def add_to_music(user_id, title, fileid_or_url, is_file_id, url_id):
     # Проверяем, существует ли пользователь
     if not get_user_by_id(user_id):
         print(f"Ошибка: пользователь с user_id {user_id} не найден!")
         return
 
     cursor.execute(
-        "INSERT INTO music (user_id, title, file_id, url_id) VALUES (?, ?, ?, ?)",
-        (user_id, title, file_id, url_id),
+        "INSERT INTO music (user_id, title, fileid_or_url, is_file_id, url_id) VALUES (?, ?, ?, ?, ?)",
+        (user_id, title, fileid_or_url, is_file_id, url_id),
     )
     conn.commit()
     print(f"Музыка '{title}' добавлена для пользователя {user_id}.")
